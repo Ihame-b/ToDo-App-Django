21d10f238a5e7240dd4fbd4f8ac47dd2550488ac
Ihame-b <ihamegrbt@gmail.com>
2023-11-25 23:12:55 +0200

    collecting error

--- MyToDoList/settings.py
+++ MyToDoList/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-scl(x8g%4!#=+%^e!!t6mo1!vywj1p@b*7k%(2g3&!0@nhr5ai
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['mjmacarty.pythonanywhere.com']
+ALLOWED_HOSTS = ['ihame.pythonanywhere.com']
 
 
 # Application definition
